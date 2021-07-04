99c7afad752b25b038a982d7d816487d9bb9ae95
BarCzerw <czerwonkabartlomiej@gmail.com>
2021-07-04 09:54:02 +0200

    Initial commit

--- src/main/java/com/sda/testing/controller/EmployeeController.java
+++ src/main/java/com/sda/testing/controller/EmployeeController.java
@@ -1,12 +1,19 @@
 package com.sda.testing.controller;
 
+import com.sda.testing.exception.InvalidOperation;
+import com.sda.testing.model.Employee;
+import com.sda.testing.model.EmployeeLevel;
 import com.sda.testing.repository.EmployeeRepository;
 import com.sda.testing.service.CompanyService;
 import com.sda.testing.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
@@ -17,4 +24,23 @@ public class EmployeeController {
     //  - szukanie pracowników po pensjach
     //  - daj podwyżkę
     //  - daj awans (promotion) pracownikowi
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/level/{level}")
+    public List<Employee> getAllEmployeesByLevel(@PathVariable(required = false) EmployeeLevel level) {
+        return employeeService.findAllFrom(level);
+    }
+
+    @GetMapping("/salary")
+    public List<Employee> getAllEmployeesBySalary(
+            @RequestParam(required = false) Double salaryFrom,
+            @RequestParam(required = false) Double salaryTo)
+    {
+            return employeeService.findAllBySalary(salaryFrom, salaryTo);
+    }
+
 }
--- src/main/java/com/sda/testing/service/EmployeeService.java
+++ src/main/java/com/sda/testing/service/EmployeeService.java
@@ -9,6 +9,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -18,39 +20,69 @@ public class EmployeeService {
 
     /**
      * List all employees.
+     *
      * @return list of employees.
      */
-    public List<Employee> findAll(){
-        return null;
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
     }
 
     /**
      * List all employees who are of given employee type.
+     *
      * @param level - type, can be null, then all employees should be returned.
      * @return list of employees with same EmployeeLevel as provided in parameter.
      */
-    public List<Employee> findAllFrom(EmployeeLevel level){
-
-        return null;
+    public List<Employee> findAllFrom(EmployeeLevel level) {
+        Optional<EmployeeLevel> levelOptional = Optional.ofNullable(level);
+        if (levelOptional.isPresent()) {
+            return employeeRepository.findAllByLevel(level);
+        } else {
+            return findAll();
+        }
     }
 
     /**
      * Find employees by salary.
+     *
      * @param salaryFrom - lower bound of salary. Can be null, then should be ignored.
-     * @param salaryTo - upper bound of salary. Can be null, then should be ignored.
+     * @param salaryTo   - upper bound of salary. Can be null, then should be ignored.
      * @return list of employees which salary is between #salaryFrom and #salaryTo
      */
-    public List<Employee> findAllBySalary(Double salaryFrom, Double salaryTo){
-        return null;
+    public List<Employee> findAllBySalary(Double salaryFrom, Double salaryTo) {
+
+        Optional<Double> lowerBoundOptional = Optional.ofNullable(salaryFrom);
+        Optional<Double> upperBoundOptional = Optional.ofNullable(salaryTo);
+
+        double lowerBound = lowerBoundOptional.orElse(0d);
+        double upperBound = upperBoundOptional.orElse(0d);
+
+        return findAll().stream().filter(employee ->
+                employee.getSalary() > lowerBound && employee.getSalary() < upperBound
+        ).collect(Collectors.toList());
+
     }
 
     /**
      * Give raise to employee found by Id.
-     * @param employeeId - employee identifier, can't be null.
+     *
+     * @param employeeId         - employee identifier, can't be null.
      * @param salaryRaisePercent - percentage of salary raise. Value can't be lower than -5 and higher than 100.
      * @throws InvalidOperation - if values of percentage or employee id is not provided, exception will be thrown.
      */
     public void giveRaise(Long employeeId, double salaryRaisePercent) throws InvalidOperation {
+        if (employeeId == null || salaryRaisePercent < -5 || salaryRaisePercent > 100) {
+            throw new InvalidOperation();
+        } else {
+            Optional<Employee> employeeOptional = employeeRepository.findById(employeeId);
+            if (employeeOptional.isPresent()) {
+                Employee employee = employeeOptional.get();
+                employee.setSalary(employee.getSalary() * (1 + (salaryRaisePercent / 100)));
+                employeeRepository.save(employee);
+            } else {
+                throw new InvalidOperation();
+            }
+        }
 
     }
 
@@ -61,14 +93,38 @@ public class EmployeeService {
      * - MANAGER -> EXECUTIVE
      * - SALES -> MANAGER
      * - ACCOUNTING -> MANAGER
-     *
+     * <p>
      * Independent employee cannot be promoted. Each promotion results in 5% net raise.
      * Promotion of manager to executive results in 3% raise.
      *
      * @param employeeId - identifier of promoted employee.
      * @throws InvalidOperation - if operation should not succeed, exception will be thrown.
      */
-    public void givePromotion(Long employeeId) throws InvalidOperation{
+    public void givePromotion(Long employeeId) throws InvalidOperation {
+        Optional<Employee> employeeOptional = employeeRepository.findById(employeeId);
+        if (employeeOptional.isPresent()) {
+            Employee employee = employeeOptional.get();
 
+            switch (employee.getLevel()) {
+                case WORKER:
+                    employee.setLevel(EmployeeLevel.LEAD);
+                    giveRaise(employeeId, 5);
+                    break;
+                case LEAD:
+                case SALES:
+                case ACCOUNTING:
+                    employee.setLevel(EmployeeLevel.MANAGER);
+                    giveRaise(employeeId, 5);
+                    break;
+                case MANAGER:
+                    employee.setLevel(EmployeeLevel.EXECUTIVE);
+                    giveRaise(employeeId, 3);
+                    break;
+                case EXECUTIVE:
+                case INDEPENDENT:
+                    throw new InvalidOperation();
+            }
+
+        }
     }
 }
